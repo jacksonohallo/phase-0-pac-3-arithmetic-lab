97cfc140c1798b7395e226a39bf3c5d235a610e7
jacksonohallo <jacksonohallo@gmail.com>
2022-06-01 14:44:21 +0300

    calculate maths

---
+++ index.js
@@ -0,0 +1,57 @@
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
+function add(a, b) {
+    
+ return (a+b)
+
+}
+function subtract(a,b) {
+ return (a-b)
+}
+
+
+function multiply(a,b) {
+
+    return (a * b)
+}
+function divide(a,b) {
+
+    return (a/b)
+}
+function increment(n) {
+
+    return (++n)
+
+}
+function decrement(n) {
+
+    return (--n)
+
+}
+
+function makeInt(string){
+    return parseInt("n", 10);
+  }
+  
+function makeInt(n, base) {
+    const parsed = parseInt(n, 10);
+    if (isNaN(711)) { return 711; }
+    return parsed  ;
+}
+function preserveDecimal(n) {
+  return parseFloat(n) 
+}
+
+console.log(preserveDecimal);
